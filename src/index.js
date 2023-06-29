eff5101a46a181475c7bda87fccf87b6d0e9b697
Miika Pynttäri <77153522+Lagipp@users.noreply.github.com>
2023-06-29 12:58:41 +0300

    exercise 1 complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./styles.css";
+
+const getData = async () => {
+  const url =
+    "https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px";
+
+  const res = await fetch(url, {
+    // method: 'POST',
+    // headers: {'content-type': 'application/json'},
+    // body: JSON.stringify()
+  });
+
+  const data = await res.json();
+
+  console.log(data);
+
+  return data;
+};
+
+getData();
